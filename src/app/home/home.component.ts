6e188676de77ea864b0e85e9b6109f46faa72d87
Dominik Skąpski <dominiksk47@gmail.com>
2023-03-01 12:51:42 +0100

    added example chart

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {Chart} from "chart.js/auto";
 
 @Component({
   selector: 'app-home',
@@ -10,5 +11,37 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.renderChart();
+  }
+
+  renderChart(){
+    const chart = new Chart("chart", {
+      type: 'doughnut',
+      data: {
+        labels: ['Przykład 1', 'Przykład 2', 'Przykład 3'],
+        datasets: [{
+          label: 'Doughnut chart example',
+          data: [10, 20, 30],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'bottom',
+          },
+          title: {
+            display: true,
+            text: 'Example Doughnut Chart'
+          }
+        }
+      }
+    });
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,9 @@
 export const environment = {
   production: false,
   // link: 'http://192.168.50.158:8080'
-  // link: 'http://192.168.43.138:8080'
+  link: 'http://192.168.43.138:8080'
   // link: 'http://192.168.50.226:8080'
-  link: 'http://192.168.1.89:8080'
+  // link: 'http://192.168.1.89:8080'
 };
 
 /*
