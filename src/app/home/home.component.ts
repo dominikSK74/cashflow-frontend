47d30b89cf003da44de6f5a8d96b9ea5a89af5cd
Dominik Skąpski <dominiksk47@gmail.com>
2023-03-09 13:05:08 +0100

    homepage customization month enum

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import {Chart} from "chart.js/auto";
+import {MonthEnum} from "../enums/month-enum";
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,12 @@ import {Chart} from "chart.js/auto";
 })
 export class HomeComponent implements OnInit {
 
+  month: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+    this.setCurrentMonth();
     this.renderChart();
   }
 
@@ -18,22 +22,24 @@ export class HomeComponent implements OnInit {
     const chart = new Chart("chart", {
       type: 'doughnut',
       data: {
-        labels: ['Przykład 1', 'Przykład 2', 'Przykład 3'],
+        labels: ['Przykład 1', 'Przykład 2', 'Przykład 3', 'Przykład 4', 'Przykład 5', 'Przykład 6', 'Przykład 7'],
         datasets: [{
-          label: 'Doughnut chart example',
-          data: [10, 20, 30],
+          label: 'Cost: ',
+          data: [10.51, 20, 30, 25],
           backgroundColor: [
             'rgb(255, 99, 132)',
             'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
+            'rgb(255, 205, 86)',
+            'rgb(255, 100, 32)'
           ],
           hoverOffset: 4
         }]
       },
       options: {
-        responsive: true,
+        responsive: false,
         plugins: {
           legend: {
+            display: true,
             position: 'bottom',
           },
           title: {
@@ -43,5 +49,15 @@ export class HomeComponent implements OnInit {
         }
       }
     });
+  }
+
+  getCurrentMonth(){
+    const date = new Date();
+    return date.getMonth();
+  }
+
+  setCurrentMonth(){
+    const monthIndex = this.getCurrentMonth();
+    this.month = MonthEnum[monthIndex];
   }
 }
